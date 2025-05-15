70d7753191f71b9c5f040a3fe03fb3b176dce45d
DanishJamian <d4nish003@gmail.com>
2025-05-15 15:08:25 +0530

    Fix title typo in Timeline component from "Accreditations & Rankings" to "Accreditations & Ranking"

--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -29,7 +29,7 @@ export const Timeline = ({ data }) => {
     >
       <div className="max-w-7xl mx-auto pt-20 px-4 md:px-8 lg:px-10">
         <h2 className="text-xl font-bold md:text-5xl text-black dark:text-white max-w-4xl">
-          Accreditations & Rankings
+          Accreditations & Ranking
         </h2>
         {/* <p className="text-neutral-700 dark:text-neutral-300 text-lg max-w-screen-md">
           I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
