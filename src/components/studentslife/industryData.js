447977f82e93580bc3968e4a7509492aa8860cf9
DanishCodebase <ammarsaifi5376@gmail.com>
2025-06-29 23:22:16 +0530

    Add new student achievement images and update industryData component
    - Introduced multiple new images showcasing student achievements, enhancing the visual representation in the application.
    - Updated the industryData component to utilize the newly added images, improving the overall presentation of student accomplishments.

--- src/components/studentslife/industryData.js
+++ src/components/studentslife/industryData.js
@@ -1,28 +1,36 @@
+import studentAchievement from "../../assets/studentslife/student'sAchievements/student-acheivement.jpg";
+import whatsAppImg1 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-1-768x512.jpeg";
+import whatsAppImg2 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-2-768x576.jpeg";
+import whatsAppImg3 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-3-e1696234510960-768x538.jpeg";
+import whatsAppImg4 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-4-768x576.jpeg";
+import whatsAppImg5 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-5-768x512.jpeg";
+import whatsAppImg6 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-6-e1696234484133-768x544.jpeg";
+import whatsAppImg7 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-7-768x576.jpeg";
+import whatsAppImg8 from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-09-26-at-10.13.13-AM-8-768x512.jpeg";
+import whatsAppImgPM from "../../assets/studentslife/student'sAchievements/WhatsApp-Image-2023-10-07-at-6.14.47-PM.jpeg";
+import whatsAppImg9 from "../../assets/studentslife/student'sAchievements/aa.png";
+
 export const industryData = [
   {
     id: 1,
-    image:
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-10-07-at-6.14.47-PM.jpeg",
+    image: whatsAppImgPM,
     date: "October 25, 2022",
     category: "education",
     title:
-      "Mr. Prakhar Singh, a 2023-25 Batch student has National Winner at the ‘Oxane Fiscal Flex Off’, the Pan India Case Study Challenge of Oxane Partners. He also received the PPI (Internship) from Oxane Partners.",
+      "Mr. Prakhar Singh, a 2023-25 Batch student has National Winner at the 'Oxane Fiscal Flex Off', the Pan India Case Study Challenge of Oxane Partners. He also received the PPI (Internship) from Oxane Partners.",
     tags: ["Finance", "Stock Market", "Learning"],
-    description: `TSSIM Student Wins @ Oxane Fiscal Flex Off: The Case Study Challenge Organized by Oxane Partners The month of September brought in a fresh bout of good news for the SSIM campus when Prakhar Singh, student of 33rd Batch of PGDM Triple Specialisation (2023-25), became the National Winner at the ‘Oxane Fiscal Flex Off’, the Pan India Case Study Challenge of Oxane Partners.
+    description: `TSSIM Student Wins @ Oxane Fiscal Flex Off: The Case Study Challenge Organized by Oxane Partners The month of September brought in a fresh bout of good news for the SSIM campus when Prakhar Singh, student of 33rd Batch of PGDM Triple Specialisation (2023-25), became the National Winner at the 'Oxane Fiscal Flex Off', the Pan India Case Study Challenge of Oxane Partners.
 
 Oxane Partners is a reputed company operating in the Financial Segment providing various services including deal pre-screening, due diligence, post-closure data management, portfolio management, surveillance among others.
 
 Prakhar Singh, a Science Graduate, joined the institute just two months back. The opportunity knocked the doors of SSIM and Prakhar was one of many students grabbing a chance to get trained and participate in the competition. Being new to the field of Finance, he pushed himself and with the help of Faculty Mentors @ SSIM and various technology platforms, he is today the National Winner of the competition.
 
 Kudos Prakhar…. SSIM Family is Proud of You!!!`,
-    gallery: [
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-10-07-at-6.14.47-PM.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/aa.png",
-    ],
+    gallery: [whatsAppImgPM, whatsAppImg9],
   },
   {
     id: 2,
-    image: "https://ssim.ac.in/wp-content/uploads/student-acheivement.jpg",
+    image: studentAchievement,
     date: "November 15, 2022",
     category: "education",
     title:
@@ -30,17 +38,16 @@ Kudos Prakhar…. SSIM Family is Proud of You!!!`,
     tags: ["Beverage", "Innovation", "Learning"],
     description: `National Youth Congress on Indian Knowledge System (NYCIKS) is a conference organized by IIT Roorkee in collaboration with Indian Knowledge Systems (IKS), an innovative cell under the Ministry of Education (MoE) at AICTE, New Delhi. SSIM student R. Bhavani Shankar of PGDM-BIFS Batch of 2022-24 participated in the Poster Presentation competition at the conference.
 
-His research paper titled “A Systematic Review on the Impact of Vidura Neethi on Personal Growth and Well-Being” under Theme 5 of the Conference – ‘Indic Perspectives on Economics, Management, Public Administration, and Governance’ received the Best Poster Presentation Award among 400+ research paper presentations at the Conference. R. Bhavani Shankar received the award from the Honourable AICTE Chairman and Ex- alumni of IIT serving the Government of India.
+His research paper titled "A Systematic Review on the Impact of Vidura Neethi on Personal Growth and Well-Being" under Theme 5 of the Conference – 'Indic Perspectives on Economics, Management, Public Administration, and Governance' received the Best Poster Presentation Award among 400+ research paper presentations at the Conference. R. Bhavani Shankar received the award from the Honourable AICTE Chairman and Ex- alumni of IIT serving the Government of India.
 
-Amidst the accolades and praises, Bhavani Shankar doesn’t forget to thank his SSIM faculty mentors for guiding him towards conceptualizing, presenting, and winning this prestigious award.
+Amidst the accolades and praises, Bhavani Shankar doesn't forget to thank his SSIM faculty mentors for guiding him towards conceptualizing, presenting, and winning this prestigious award.
 
 Kudos Bhavani Shankar… SSIM is very proud of you!!!`,
     gallery: [],
   },
   {
     id: 3,
-    image:
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-5-768x512.jpeg",
+    image: whatsAppImg5,
     date: "December 1, 2022",
     category: "education",
     title:
@@ -62,14 +69,14 @@ Visiting the rural villages and meeting the village administrators to understand
 development initiatives
 While doing this, he certainly became an integral part of the culturally rich Yuva Sangam (Youth Confluence) 2023.`,
     gallery: [
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-5-768x512.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-4-768x576.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-2-768x576.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-7-768x576.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-6-e1696234484133-768x544.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-3-e1696234510960-768x538.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-8-768x512.jpeg",
-      "https://ssim.ac.in/wp-content/uploads/WhatsApp-Image-2023-09-26-at-10.13.13-AM-1-768x512.jpeg",
+      whatsAppImg5,
+      whatsAppImg4,
+      whatsAppImg2,
+      whatsAppImg7,
+      whatsAppImg6,
+      whatsAppImg3,
+      whatsAppImg8,
+      whatsAppImg1,
     ],
   },
 ];
