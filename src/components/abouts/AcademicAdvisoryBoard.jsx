427a7b1857aece0392271700633f5af051ddf153
DanishCodebase <ammarsaifi5376@gmail.com>
2025-06-29 18:38:49 +0530

    Add new images for Academic Advisory Board members
    - Introduced multiple new images for members of the Academic Advisory Board, enhancing the visual representation of the team.
    - Updated the AcademicAdvisoryBoard component to utilize the newly added images, improving the overall presentation and consistency of member profiles.

--- src/components/abouts/AcademicAdvisoryBoard.jsx
+++ src/components/abouts/AcademicAdvisoryBoard.jsx
@@ -1,6 +1,33 @@
 import { useState } from "react";
-import Ramana from "../../assets/faculty&research/faculty/Dr. Ramana Rao.webp";
-import Ambuj from "../../assets/about/dr-ambuj.jpeg";
+import Ramana from "../../assets/about/academic-advisory-board/Dr. Ramana Rao.webp";
+import Ambuj from "../../assets/about/academic-advisory-board/dr-ambuj.jpeg";
+import ArathySampathy from "../../assets/about/academic-advisory-board/Smt_Arathy_Sampathy.jpg";
+import SaileshSampathy from "../../assets/about/academic-advisory-board/Dr_Sailesh_Sampathy.jpg";
+import AbhiramaKrishna from "../../assets/about/academic-advisory-board/Prof_Abhiram_Krishna.jpg";
+import AshokAgarwal from "../../assets/about/academic-advisory-board/Ashok_K_Agarwal.jpg";
+import AsitMohapatra from "../../assets/about/academic-advisory-board/Dr_Asit-Mohapatra.jpg";
+import Gopalakrishna from "../../assets/about/academic-advisory-board/Sri_M.-Gopalakrishna.jpg";
+import GubbaPrashanth from "../../assets/about/academic-advisory-board/Gubba_Prashant.jpg";
+import LakshmipathiItha from "../../assets/about/academic-advisory-board/Lakshmipathi_Itha.jpg";
+import MamathaM from "../../assets/about/academic-advisory-board/M_Mamatha.jpg";
+import GDMogli from "../../assets/about/academic-advisory-board/Prof-G-D-Mogli.jpg";
+import MuraliR from "../../assets/about/academic-advisory-board/Murali_Retineni.jpg";
+import PetluriSrinivas from "../../assets/about/academic-advisory-board/OIP.jpeg";
+import RaghavendraRao from "../../assets/about/academic-advisory-board/Raghavendra_Rao.jpg";
+import RamBhoopalP from "../../assets/about/academic-advisory-board/P_Rambhoopal.jpg";
+import RamachandraRao from "../../assets/about/academic-advisory-board/Mr_Ramachandra_Rao.jpg";
+import RaviShankarMeela from "../../assets/about/academic-advisory-board/Ravishankar_Meela.jpg";
+import SatishNaidu from "../../assets/about/academic-advisory-board/Satish_Naidu.jpg";
+import ShanthanReddy from "../../assets/about/academic-advisory-board/OIP.jpeg";
+import SivaKumar from "../../assets/about/academic-advisory-board/SIVA_Kumar.jpg";
+import SrikanthSurampudi from "../../assets/about/academic-advisory-board/Srikanth_Surampudi.jpg";
+import VenkaiahV from "../../assets/about/academic-advisory-board/V_Venkaiah.jpg";
+import VirmaniBR from "../../assets/about/academic-advisory-board/OIP.jpeg";
+import ViswanadhamV from "../../assets/about/academic-advisory-board/V_Viswanadham.jpg";
+import YerramRajuB from "../../assets/about/academic-advisory-board/Yerram_Naidu.jpg";
+import BhaskaraMurthyP from "../../assets/about/academic-advisory-board/Bhaskar_murthy.jpg";
+import ChariVG from "../../assets/about/academic-advisory-board/VG_CHARI.jpg";
+import SudhakarA from "../../assets/about/academic-advisory-board/OIP.jpeg";
 
 export default function AcademicAdvisoryBoard() {
   const teamMembers = [
@@ -8,15 +35,13 @@ export default function AcademicAdvisoryBoard() {
       name: "Smt. Arathy Sampathy",
       role: "President",
       bio: "President & Chief Executive, SSGI",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/09/Smt_Arathy_Sampathy.jpg",
+      image: ArathySampathy,
     },
     {
       name: "Dr. Sailesh Sampathy",
       role: "Vice President",
       bio: "Vice President & Dy. Chief Executive, SSGI",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/09/Dr_Sailesh_Sampathy.jpg",
+      image: SaileshSampathy,
     },
     {
       name: "Prof. (Dr.) Ambuj Jagdish Gupta",
@@ -34,165 +59,151 @@ export default function AcademicAdvisoryBoard() {
       name: "Prof. S. Abhirama Krishna",
       role: "Director General",
       bio: "Badruka Group of Institutions, Hyderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Prof_Abhiram_Krishna.jpg",
+      image: AbhiramaKrishna,
     },
     {
       name: "Dr. Ashok K. Agarwal",
       role: "Director",
       bio: "Director, EWB India, Member, Governing Council, Badruka Education Society, Hyderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Ashok_K_Agarwal.jpg",
+      image: AshokAgarwal,
     },
     {
       name: "Dr. Asit Mohapatra",
       role: "Professor of Practice – OB & HRM",
       bio: "Indian Institute of Management, Ranchi.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Dr_Asit-Mohapatra.jpg",
+      image: AsitMohapatra,
     },
     {
       name: "Sri. M. Gopalakrishna I.A.S. (Retd.)",
       role: "Former Chairman",
       bio: "REC, Hyderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Sri_M.-Gopalakrishna.jpg",
+      image: Gopalakrishna,
     },
     {
       name: "Sri. Gubba Prashanth",
       role: "Research & Technical Head",
       bio: "Gubba Cold Storage, Secunderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/Gubba_Prashant.jpg",
+      image: GubbaPrashanth,
     },
     {
       name: "Sri. Lakshmipathi Itha",
       role: "Human Resource Director",
       bio: "GVK BIO, Hyderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Lakshmipathi_Itha.jpg",
+      image: LakshmipathiItha,
     },
     {
       name: "Smt. Mamatha. M",
       role: "Senior Vice President",
       bio: "HSBC Bank, Hyderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/M_Mamatha.jpg",
+      image: MamathaM,
     },
     {
       name: "Dr. G.D. Mogli",
       role: "CEO",
       bio: "Dr. Mogli Health Care Management Consultancy, Hyderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/Prof-G-D-Mogli.jpg",
+      image: GDMogli,
     },
     {
       name: "Sri. Murali. R",
       role: "Executive Director",
       bio: "Celkon Mobiles Pvt Ltd, Hyderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Murali_Retineni.jpg",
+      image: MuraliR,
     },
     {
       name: "Sri. Petluri Srinivas",
       role: "Member",
       bio: "Warehousing Development and Regulatory Authority (WDRA), Delhi.",
-      image:
-        "https://th.bing.com/th/id/OIP.pA6t_4n5Y5TvS00-jLkhPwHaHa?w=500&h=500&rs=1&pid=ImgDetMain",
+      image: PetluriSrinivas,
     },
     {
       name: "Sri. Raghavendra Rao",
       role: "Director",
       bio: "Global Sales Operations (FS), Hyderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Raghavendra_Rao.jpg",
+      image: RaghavendraRao,
     },
     {
       name: "Sri. Ram Bhoopal. P",
       role: "Co-Founder",
       bio: "M/s. Riverbridge Tech Venture Pvt. Ltd, Hyderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/P_Rambhoopal.jpg",
+      image: RamBhoopalP,
     },
     {
       name: "Prof. K.S. Ramachandra Rao",
       role: "CEO",
       bio: "Visishta Consultancy, Secunderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Mr_Ramachandra_Rao.jpg",
+      image: RamachandraRao,
     },
     {
       name: "Sri. Ravi Shankar Meela",
       role: "Executive Director",
       bio: "Sudhakar PVC Products Pvt. Ltd, Nalgonda District, Telangana.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Ravishankar_Meela.jpg",
+      image: RaviShankarMeela,
     },
     {
       name: "Sri. Satish Naidu",
       role: "Founder & Chief Visionary",
       bio: "Invensure Life Insurance Pvt. Ltd, Secunderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/Satish_Naidu.jpg",
+      image: SatishNaidu,
     },
     {
       name: "Sri. C. Shanthan Reddy",
       role: "Managing Partner",
       bio: "Arise Films, Hyderabad.",
-      image:
-        "https://th.bing.com/th/id/OIP.pA6t_4n5Y5TvS00-jLkhPwHaHa?w=500&h=500&rs=1&pid=ImgDetMain",
+      image: ShanthanReddy,
     },
     {
       name: "Dr. S. Siva Kumar",
       role: "Divisional Chief Executive",
       bio: "ITC Agro Business Division, Secunderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/SIVA_Kumar.jpg",
+      image: SivaKumar,
     },
     {
       name: "Sri. Srikanth Surampudi",
       role: "General Manager – HR & Regional HR Head",
       bio: "Tata Consultancy Services Ltd, Hyderabad.",
-      image:
-        "https://ssim.ac.in/wp-content/uploads/2022/10/Srikanth_Surampudi.jpg",
+      image: SrikanthSurampudi,
     },
     {
       name: "Prof. V. Venkaiah",
       role: "Director",
       bio: "Centre for Education Technology and Learning Sciences, A.P.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/V_Venkaiah.jpg",
+      image: VenkaiahV,
     },
     {
       name: "Prof. B.R. Virmani",
       role: "Chairman",
       bio: "Centre for Organization Research & Development in Management (CORD-M), Hyderabad.",
-      image:
-        "https://th.bing.com/th/id/OIP.pA6t_4n5Y5TvS00-jLkhPwHaHa?w=500&h=500&rs=1&pid=ImgDetMain",
+      image: VirmaniBR,
     },
     {
       name: "Prof. V. Viswanadham",
       role: "Formerly Director (Academic)",
       bio: "SSIM, Secunderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/V_Viswanadham.jpg",
+      image: ViswanadhamV,
     },
     {
       name: "Prof. B. Yerram Raju",
       role: "Adviser",
       bio: "Telangana Industrial Health Clinic Ltd, Govt. of Telangana, Hyderabad.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/Yerram_Naidu.jpg",
+      image: YerramRajuB,
     },
     {
       name: "Sri. P. Bhaskara Murthy",
       role: "Independent Consultant",
       bio: "IQPRO Analytics.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/Bhaskar_murthy.jpg",
+      image: BhaskaraMurthyP,
     },
     {
       name: "Dr. V.G. Chari",
       role: "Formerly Assistant Vice President",
       bio: "SSGI.",
-      image: "https://ssim.ac.in/wp-content/uploads/2022/10/VG_CHARI.jpg",
+      image: ChariVG,
     },
     {
       name: "Prof. A. Sudhakar",
       role: "Former Director – General",
       bio: "SSGI.",
-      image:
-        "https://th.bing.com/th/id/OIP.pA6t_4n5Y5TvS00-jLkhPwHaHa?w=500&h=500&rs=1&pid=ImgDetMain",
+      image: SudhakarA,
     },
   ];
 
